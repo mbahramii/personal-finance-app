3e95c12d1658cbb19c3055f4f88dc7c541e84de6
mbahramii <bahramiimohammad92@gmail.com>
2025-08-02 01:27:39 +0330

    feat: make App layout responsive with conditional Sidebar and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Footer from "./assets/components/Footer"
+import SideBar from "./assets/components/SideBar"
 import Overview from "./assets/pages/Overview"
 
 function App() {
 
   return (
-    <div className="bg-cream ">
+    <div className="w-screen bg-cream flex flex-col lg:flex-row ">
+       <div className="hidden lg:block"> 
+        <SideBar/>
+      </div>
       <Overview />
+      <div  className="block lg:hidden">
       <Footer/>
+      </div>
+
     </div>
   )
 }
