eaad28b073d39d871587d0e1281e7d861967b0f8
mbahramii <bahramiimohammad92@gmail.com>
2025-08-02 00:02:26 +0330

    feat: add RecurringBills component with static bill summaries and status colors

---
+++ src/assets/components/RecurringBills.jsx
@@ -0,0 +1,42 @@
+import { IoMdArrowDropright } from "react-icons/io";
+
+const RecurringBills= ()=>{
+    return(
+        <div className="bg-white h-[300px] p-5 rounded-xl mt-10">
+        <div className="flex justify-between mb-10 ">
+          <p className="text-black"> Recurring Bills</p>
+          <div className="flex items-center text-gray">
+            <p className="">see details</p>
+            <h1>
+              <IoMdArrowDropright />
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center pb-14">
+          <div className="flex items-center bg-[#f7f3ec] rounded-lg w-[95%] relative my-4">
+            <div className="w-1.5 h-full bg-teal-700 rounded-l-md  absolute"></div>
+            <div className="flex justify-between w-full items-center p-3">
+              <p className="text-sm text-gray-800">Paid Bills</p>
+              <p className="text-sm font-semibold text-gray-900">$190.00</p>
+            </div>
+          </div>
+          <div className="flex items-center bg-[#f7f3ec] rounded-lg w-[95%] relative my-4">
+            <div className="w-1.5 h-full bg-cyan-400 rounded-l-md  absolute"></div>
+            <div className="flex justify-between w-full items-center p-3">
+              <p className="text-sm text-gray-800">Total Upcoming</p>
+              <p className="text-sm font-semibold text-gray-900">$190.00</p>
+            </div>
+          </div>
+          <div className="flex items-center bg-[#f7f3ec] rounded-lg w-[95%] relative my-4">
+            <div className="w-1.5 h-full bg-amber-400 rounded-l-md  absolute"></div>
+            <div className="flex justify-between w-full items-center p-3">
+              <p className="text-sm text-gray-800">Due Soon</p>
+              <p className="text-sm font-semibold text-gray-900">$190.00</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default RecurringBills;
