bc98bce5cf7d62a2d8d08cad323140a44cec6323
mbahramii <bahramiimohammad92@gmail.com>
2025-08-02 00:04:18 +0330

    feat: enhance Footer component with labels and responsive vertical layout for large screens

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -6,21 +6,40 @@ import { GrMoney } from "react-icons/gr";
 
 const Footer = () => {
   return (
-    <div className="w-full flex gap-10 justify-center bg-neutral-600 rounded-t-xl h-[50px] items-center fixed bottom-0">
+    <div className="w-full flex gap-10 justify-center bg-neutral-600 rounded-t-xl h-[50px] items-center fixed bottom-0 text-white lg:flex-col">
       {/* <Link to="/home"> */}
-        <MdHome className="w-6 h-6 text-white hover:text-blue-400 transition" />
+      <div className="flex flex-col text-center items-center">
+        <MdHome className=" w-6 h-6 text-white hover:text-blue-400 transition" />
+        <h1 className="hidden sm:block ">Overview</h1>
+      </div>
       {/* </Link> */}
       {/* <Link to="/transfer"> */}
+      <div className="flex flex-col items-center">
+
         <BiTransfer className="rotate-90 w-6 h-6 text-white hover:text-blue-400 transition" />
+        <h1 className="hidden sm:block ">Transaction</h1>
+      </div>
       {/* </Link> */}
       {/* <Link to="/analytics"> */}
+      <div className="flex flex-col items-center">
+
         <GrAnalytics className="w-6 h-6 text-white hover:text-blue-400 transition" />
+        <h1 className="hidden sm:block ">Budgets</h1>
+      </div>
       {/* </Link> */}
       {/* <Link to="/income"> */}
+      <div className="flex flex-col items-center">
+
         <GrMoney className="w-6 h-6 text-white hover:text-blue-400 transition" />
+        <h1 className="hidden sm:block ">Pots</h1>
+      </div> 
       {/* </Link> */}
       {/* <Link to="/receipts"> */}
+      <div className="flex flex-col items-center">
+
         <PiReceiptLight className="w-6 h-6 text-white hover:text-blue-400 transition" />
+        <h1 className="hidden sm:block ">Recurring Bills</h1>
+      </div>
       {/* </Link> */}
     </div>
   );
