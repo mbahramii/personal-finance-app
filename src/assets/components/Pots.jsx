430f51a7a0911431c580588d8c0bea7a46952d29
mbahramii <bahramiimohammad92@gmail.com>
2025-08-02 13:20:33 +0330

    refactor: improve Pots component responsiveness and layout

--- src/assets/components/Pots.jsx
+++ src/assets/components/Pots.jsx
@@ -15,34 +15,34 @@ return(
               </div>
             </div>
             <div className="flex flex-col items-center justify-center sm:flex sm:flex-row sm:justify-between">
-              <div className="bg-cream rounded-[10px] w-[270px] h-[120px] p-10">
+              <div className="bg-cream rounded-[10px] w-[270px]  h-[120px] p-10 lg:w-[140px]">
                 <div>total saved</div>
                 <GrMoney />
               </div>
-              <div className="flex flex-wrap items-center  justify-center w-[300px] mt-4 ">
-                <div className="min-w-[130px] flex flex-row items-center mb-5">
-                  <span className="w-1 h-10 bg-emerald-700 mr-2 rounded-xl"></span>
+              <div className="flex flex-wrap items-center  justify-end w-[250px] mt-4 ">
+                <div className="min-w-[125px] flex flex-row items-center mb-5">
+                  <span className="w-1 h-10 bg-emerald-700 mr-2 lg:mr-px rounded-xl"></span>
                   <div className="flex flex-col">
                     <span className="text-gray">Savings</span>
                     <span>$159</span>
                   </div>
                 </div>
-                <div className="min-w-[130px] flex flex-row items-center mb-5">
-                  <span className="w-1 h-10 bg-sky-400 mr-2  rounded-xl"></span>
+                <div className="min-w-[110px] flex flex-row items-center mb-5">
+                  <span className="w-1 h-10 bg-sky-400 mr-2 lg:mr-px rounded-xl"></span>
                   <div className="flex flex-col">
                     <span className="text-gray">Gift</span>
                     <span>$40</span>
                   </div>
                 </div>
-                <div className="min-w-[130px] flex flex-row items-center mb-5">
-                  <span className="w-1 h-10 bg-purple-800 mr-2 rounded-xl"></span>
+                <div className="min-w-[125px] flex flex-row items-center mb-5">
+                  <span className="w-1 h-10 bg-purple-800 mr-pmr-2 lg:mr-px rounded-xl"></span>
                   <div className="flex flex-col">
                     <span className="text-gray">Concert Ticket</span>
                     <span>$110</span>
                   </div>
                 </div>
-                <div className="min-w-[130px] flex flex-row items-center mb-5">
-                  <span className="w-1 h-10 bg-orange-400 mr-2 rounded-xl"></span>
+                <div className="min-w-[110px] flex flex-row items-center mb-5">
+                  <span className="w-1 h-10 bg-orange-400 mr-2 lg:mr-px rounded-xl"></span>
                   <div className="flex flex-col">
                     <span className="text-gray">New laptop</span>
                     <span>$10</span>
