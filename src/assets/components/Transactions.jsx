14dbb7fef5d4b905895eeeab2ddd49dc5f2eadd1
mbahramii <bahramiimohammad92@gmail.com>
2025-08-04 00:05:58 +0330

    fix(transactions): add transactions prop handling to Transactions component

--- src/assets/components/Transactions.jsx
+++ src/assets/components/Transactions.jsx
@@ -1,42 +1,35 @@
 import { IoMdArrowDropright } from "react-icons/io";
 
-const Transactions=()=>{
-
-return(
-
+const Transactions = ({ transactions }) => {
+  return (
     <div className="bg-white p-5 rounded-xl mt-10">
-            <div className="flex justify-between  ">
-              <p>Transactions</p>
-              <div className="flex items-center text-gray">
-                <h1>View All </h1>
-                <h1>
-                  <IoMdArrowDropright />
-                </h1>
-              </div>
-            </div>
-            <div className=" p-4 flex justify-between">
-              <h1>Emma Richardson</h1>
-              <div>
-                <h1 className="text-emerald-700">+$75.50</h1>
-                <h1>19 Aug 2024</h1>
-              </div>
-            </div>
-            <div className=" p-4 flex justify-between">
-              <h1>Emma Richardson</h1>
-              <div>
-                <h1 className="text-emerald-700">+$75.50</h1>
-                <h1>19 Aug 2024</h1>
-              </div>
-            </div>
-            <div className=" p-4 flex justify-between">
-              <h1>Emma Richardson</h1>
-              <div>
-                <h1 className="text-emerald-700">+$75.50</h1>
-                <h1>19 Aug 2024</h1>
-              </div>
-            </div>
-            <hr className="bg-gray w-full" />
+      <div className="flex justify-between  ">
+        <p>Transactions</p>
+        <div className="flex items-center text-gray">
+          <h1>View All </h1>
+          <h1>
+            <IoMdArrowDropright />
+          </h1>
+        </div>
+      </div>
+{/* Transaction list */}
+      {transactions.map((tx, index) => (
+        <div key={index} className=" p-4 flex justify-between">
+          <h1>{tx.name}</h1>
+          <div>
+            <h1
+              className={`font-semibold ${
+                tx.type === "deposit" ? "text-emerald-600" : "text-red-500"
+              }`}
+            >
+              ${tx.amount}
+            </h1>
+            <h1>{tx.date}</h1>
           </div>
-)
-}
+          {/* <hr className="bg-gray w-full" /> */}
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Transactions;
