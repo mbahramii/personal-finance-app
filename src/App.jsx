f3e6d108d9eed126b2c06ef2cbddba8c9813f47e
mbahramii <bahramiimohammad92@gmail.com>
2025-08-05 19:56:44 +0330

    Add React Router and setup basic routing for Overview & Transactions pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./assets/components/Footer"
 import SideBar from "./assets/components/SideBar"
-import Overview from "./assets/pages/Overview"
+import Overviewpage from "./assets/pages/Overviewpage"
+import Transactionspage from "./assets/pages/Transactionspage"
 
 function App() {
 
   return (
+      <BrowserRouter>
     <div className="w-screen h-auto bg-cream flex flex-col lg:flex-row ">
        <div className="hidden lg:block overflow-y-auto h-screen w-60 "> 
         <SideBar/>
       </div>
-      <div className="flex-1 h-screen overflow-y-auto"> 
-      <Overview />
-      </div>
+
+      <Routes>
+        <Route path="/" element={<Overviewpage/>} />
+        <Route path="/Transactionspage" element={<Transactionspage/>} />
+      </Routes>
       <div  className="block lg:hidden">
       <Footer/>
       </div>
-
     </div>
+    </BrowserRouter>
   )
+
 }
 
 export default App
