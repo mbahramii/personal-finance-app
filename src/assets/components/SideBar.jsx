ed2bdd1f04402808ab0dfbe127cacdc855005315
mbahramii <bahramiimohammad92@gmail.com>
2025-08-09 13:57:24 +0330

    feat: highlight active links in Sidebar navigation

--- src/assets/components/SideBar.jsx
+++ src/assets/components/SideBar.jsx
@@ -3,21 +3,28 @@ import { BiTransfer } from "react-icons/bi";
 import { GrAnalytics } from "react-icons/gr";
 import { PiReceiptLight } from "react-icons/pi";
 import { GrMoney } from "react-icons/gr";
-import { Link } from "react-router-dom";
+import { Link ,useLocation} from "react-router-dom";
 
 const SideBar = () => {
+const location = useLocation();
+const isActive = (path) => location.pathname === path;
+
   return (
     <div className="h-screen flex gap-10 bg-neutral-600 items-start text-white lg:flex-col p-12 pb-96">
       <Link to="/">
-      <div className="flex flex-row gap-2 items-end justify-end">
-        <MdHome className=" w-6 h-6 text-white hover:text-blue-400 transition" />
+      <div className={`flex flex-row gap-2 items-end justify-end ${
+    isActive("/") ? "text-blue-400" : "text-white hover:text-blue-400"
+  }`}>
+        <MdHome className=" w-6 h-6  hover:text-blue-400 transition" />
         <h1 className="">Overview</h1>
       </div>
       </Link>
       <Link to="/Transactionspage">
-      <div className="flex flex-row gap-2 items-end justify-end">
-        <BiTransfer className="rotate-90 w-6 h-6 text-white hover:text-blue-400 transition" />
-        <h1 className="">Transaction</h1>
+      <div className={`flex flex-row gap-2 items-end justify-end  ${
+    isActive("/Transactionspage") ? "text-blue-400" : "text-white hover:text-blue-400"
+  }`}>
+        <BiTransfer className="rotate-90 w-6 h-6  hover:text-blue-400 transition" />
+        <h1 className="">Transactions</h1>
       </div>
       </Link>
       {/* <Link to="/analytics"> */}
