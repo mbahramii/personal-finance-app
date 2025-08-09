b2e313841e057e48d90d84ec17a925fcc4e6a3db
mbahramii <bahramiimohammad92@gmail.com>
2025-08-09 13:57:08 +0330

    feat: highlight active links in Footer navigation

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -3,19 +3,27 @@ import { BiTransfer } from "react-icons/bi";
 import { GrAnalytics } from "react-icons/gr";
 import { PiReceiptLight } from "react-icons/pi";
 import { GrMoney } from "react-icons/gr";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
-  return (
+
+const location = useLocation();
+const isActive = (path) => location.pathname === path;
+
+return (
     <div className="w-full flex gap-10 justify-center bg-neutral-600 rounded-t-xl h-[50px] items-center fixed bottom-0 text-white lg:flex-col">
       <Link to="/">
-      <div className="flex flex-col text-center items-center">
+      <div className={`flex flex-col text-center items-center ${
+    isActive("/") ? "text-blue-400" : "text-white hover:text-blue-400"
+  }`}>
         <MdHome className=" w-6 h-6 text-white hover:text-blue-400 transition" />
         <h1 className="hidden sm:block ">Overview</h1>
       </div>
       </Link>
       <Link to="/Transactionspage">
-      <div className="flex flex-col items-center">
+      <div className={`flex flex-col items-center ${
+    isActive("/") ? "text-blue-400" : "text-white hover:text-blue-400"
+  }`}>
         <BiTransfer className="rotate-90 w-6 h-6 text-white hover:text-blue-400 transition" />
         <h1 className="hidden sm:block ">Transaction</h1>
       </div>
